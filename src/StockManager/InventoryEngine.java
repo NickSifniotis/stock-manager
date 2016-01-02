ece24c254f16dcdc3300c1d7487c5243316efb7b
nsifniotis <u5809912@anu.edu.au>
2016-01-02 13:06:29 +1100

    Adding some javadocs documentation to the codes

--- src/StockManager/InventoryEngine.java
+++ src/StockManager/InventoryEngine.java
@@ -3,49 +3,101 @@ package StockManager;
 import StockManager.Objects.Item;
 import StockManager.Objects.ShoppingTuple;
 import StockManager.Objects.StockRecord;
-import StockManager.SimpleDatabase.DataObject;
 import StockManager.SimpleDatabase.SimpleDB;
 
-import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
+
 
 /**
  * <p>
- *     Static class that contains a variety of useful functions.
+ *     Static class that contains a variety of useful helper functions.
  * </p>
  *
+ * @author Nick Sifniotis u5809912
+ * @since 31/12/2015
+ * @version 1.0.0
  */
 public class InventoryEngine
 {
-    public static Date ParseDate(String unparsed_date)
-    {
-        SimpleDateFormat sdfmt1 = new SimpleDateFormat("dd/MM/yy");
-        Date res = null;
-        try
-        {
-            res = sdfmt1.parse(unparsed_date);
-        }
-        catch (Exception e)
-        {
-            // @TODO make this meaningful
-        }
-        return res;
-    }
-
-
+    /**
+     * <p>
+     *     Adds a stocktake record for an item into the database. The record is built based on the parameters
+     *     passed into this method.
+     * </p>
+     *
+     * <p>
+     *     Stocktake records are 'snapshots in time' of how many <i>items</i> were on hand at a particular date.
+     *     The quantity of items on hand can be zero, but it can never be negative. If a negative quantity is
+     *     encountered, this method will fail silently.
+     * </p>
+     *
+     * @param item The item that this record relates to.
+     * @param quantity The number of items that were on hand at the time of the stocktake. If this parameter is
+     *                 less than zero, this method fails silently.
+     * @param date The date of the stocktake. This field is computed using the Stocktake.Calendar static methods.
+     *             Do not try and peer inside this abstraction, just trust the Calendar to do its thing.
+     */
     public static void AddStocktakeRecord(Item item, int quantity, int date)
     {
-        __add_record(item, quantity, date, false);
+        if (quantity >= 0)
+            __add_record(item, quantity, date, false);
     }
 
 
+    /**
+     * <p>
+     *     Adds a restock record for an item into the database. The record is built based on the parameters
+     *     passed into this method.
+     * </p>
+     *
+     * <p>
+     *     A restocking event is ostensibly an alteration of an item's stock level due to a purchase of more items,
+     *     but it can be used for any extraordinary event which alters the stock levels of an item. For example,
+     *     giving ten rolls of toilet paper away to a poor friend in need (?) will change the amount of toilet
+     *     paper available, but it will not affect the usage rate of that item.
+     * </p>
+     *
+     * <p>
+     *     Usage rates are computed based on movements in stock levels that are not accounted for by these restock
+     *     records. Therefore, there are no restrictions imposed on the quanitity parameter. A negative stock
+     *     shift could be the result of giving away items, or loaning them, or shrinkage, etc ..
+     * </p>
+     *
+     * @param item The item that this record relates to.
+     * @param quantity The change in the number of items on hand. A positive change could signify the purchase of
+     *                 more items, while a negative could signify theft, spoilage or loan of items.
+     * @param date The date of the restock. This field is computed using the Stocktake.Calendar static methods.
+     *             Do not try and peer inside this abstraction, just trust the Calendar to do its thing.
+     */
     public static void AddRestockRecord(Item item, int quantity, int date)
     {
         __add_record(item, quantity, date, true);
     }
 
 
+    /**
+     * <p>
+     *     Computes the rate of consumption of all items for which there is sufficient information available.
+     * </p>
+     *
+     * <p>
+     *     This method really gets to the heart of what this application is about. Using the stocktake and
+     *     restock records created by the AddStocktakeRecord and AddRestockRecord methods, ComputeConsumption
+     *     calculates the rate at which items are being consumed and returns this data, along with the most up-to-date
+     *     stock counts for each item.
+     * </p>
+     *
+     * <p>
+     *     The consumption rate is the quantity of the item that is consumed per day. This value is computed by
+     *     dividing the change in an items stock level by the number of days that the change in stock was measured
+     *     over.
+     * </p>
+     *
+     * @param items The Items for which we want to calculate the rates of consumption.
+     * @return A List of ShoppingTuple instances. The ShoppingTuple class contains references to an Item,
+     *         the current quantity of the item on hand (based on the last StockRecord available) and the
+     *         rate of consumption of the item, as a double.
+     */
     public static List<ShoppingTuple> ComputeConsumption(Item[] items)
     {
         List<ShoppingTuple> res = new LinkedList<>();
@@ -111,6 +163,18 @@ public class InventoryEngine
     }
 
 
+    /**
+     * <p>
+     *     Inserts a new StockRecord into the database. The StockRecord is created based on the parameters
+     *     passed into this method.
+     * </p>
+     *
+     * @param item The item that this record refers to.
+     * @param quantity If this is a stocktake record, the number of items on hand. If this is a restocking record,
+     *                 the number of items that have been purchased.
+     * @param date The date of the restock / stocktake.
+     * @param is_restock TRUE if this record is for a restocking event, FALSE if it is for a stocktake event.
+     */
     private static void __add_record(Item item, int quantity, int date, boolean is_restock)
     {
         StockRecord new_record = (StockRecord) SimpleDB.New(StockRecord.class);
